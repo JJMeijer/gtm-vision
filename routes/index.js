523dfe781196aa55c5b2efc24ec305bc409b070f
tserest <j.johanmeijer@gmail.com>
2019-02-03 20:09:55 +0100

    Form Field ready

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,39 @@
 /**
- * Express Router Init
+ * Express Router
  */
 
 import express from 'express';
+import urllib from 'urllib';
 
 const router = express.Router();
 
-/* GET Homepage */
+/* GET Main Page */
 router.get('*', (req, res) => {
   res.render('index', {
     title: 'GTM Insight',
   });
+});
+
+/* Post GTM Form */
+router.post('/api/gtm', (req, res, next) => {
+  if (req.body && req.body.value) {
+    const { value } = req.body;
+    urllib.request(`https://www.googletagmanager.com/gtm.js?id=${value}`, {
+      dataType: 'text',
+    },
+    (err, data) => {
+      if (err) {
+        next(err);
+      } else {
+        const containerText = data.match(/{\n"resource":\s{[\s\S]*,\n"runtime"/g)[0].replace(/,\n"runtime"/, '}');
+        res.json(JSON.parse(unescape(containerText)));
+      }
+    });
+  } else {
+    const error = new Error('Missing post request body');
+    error.httpStatusCode = 400;
+    next(error);
+  }
 });
 
 export default router;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
+import PropTypes from 'prop-types';
 
-const MainApp = () => (
-  <h1>Hello React!</h1>
+import FormGtm from './form';
+import './index.scss';
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+function Title({ title }) {
+  return <h1 className="title">{title}</h1>;
+}
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+const App = () => (
+  <div>
+    <Title title="Main Title" />
+    <FormGtm />
+  </div>
 );
 
-render(<MainApp />, document.getElementById('app'));
+render(<App />, document.getElementById('root'));
